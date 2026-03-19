cd07cc73f4ae4aa34b89ce54526880e7bfeb7a2a
indian-places <nandannandanh@gmail.com>
2026-03-19 21:05:54 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`SERVER HAS STARTED AT PORT ${PORT}`);
+});
